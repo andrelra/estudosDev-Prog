336d260dddb660fc7f3cf3ecca409505b5241663
André Arôncio <andrearoncio@gmail.com>
2024-04-10 21:04:40 -0400

    Correções v1 fibonacci

--- Phyton/python_modulo_2/ex-62-Fibonacci.py
+++ Phyton/python_modulo_2/ex-62-Fibonacci.py
@@ -1,15 +1,14 @@
 #Seqüência de fibonacci
-n = int(input('Digite um número para a seqüência: '))
+n = int(input('Digite um número para n primeiras triplas de números da seqüência: '))
 # casos base:
 faa = 0
 fa = 1
-res = 0
 fx = 0
-i = 1
-print('{} - {}'.format(faa,fa), end= '')
-while i != n:
-    fx = (fa) + (faa)
+i = 0
+print("{} - {} - " .format(faa, fa), end= '')
+while i != (n - 2):
+    fx = fa + faa
     faa = fx + fa
     fa = faa + fx
-    print('{} - {} - {}'.format(fx, fa, faa), end = '')
+    print('{} - {} - {} - ' .format(fx, faa, fa), end = '')
     i += 1 
